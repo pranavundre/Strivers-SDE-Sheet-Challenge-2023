e33ed439b60919eb2b704b5437105ec3c2d339a4
Pranav Undre <undrepranav@gmail.com>
2023-06-08 02:15:59 +0530

    Create 012-Count-Inversions.cpp

---
+++ 012-Count-Inversions.cpp
@@ -0,0 +1,11 @@
+long long getInversions(long long *arr, int n){
+    int  cnt=0;
+    for(int i=0;i<n;i++){
+        for(int j=i+1;j<n;j++){
+            if(arr[i]>arr[j]){
+                cnt++;
+            }
+        }
+    }
+    return cnt;
+}
